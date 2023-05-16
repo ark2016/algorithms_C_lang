6f1a4991820c11a1d1a78626520db633e769bcd9
Arkadiy <91221701+ark2016@users.noreply.github.com>
2023-05-16 23:01:54 +0300

    Create pword.c

---
+++ pword.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void prefix_function(const char *s, int *pi, size_t n) {
+    pi[0] = 0;
+    for (size_t i = 1; i < n; ++i) {
+        int j = pi[i - 1];
+        while ((j > 0) && (s[i] != s[j]))
+            j = pi[j - 1];
+        if (s[i] == s[j])
+            ++j;
+        pi[i] = j;
+    }
+}
+
+bool is_made_of_prefixes(char *string, char *t_string) {
+    int string_len, t_string_len;
+    string_len = strlen(string);
+    t_string_len = strlen(t_string);
+    int pi[string_len];
+    prefix_function(string, pi, string_len);
+    int q = 0;
+    for (size_t k = 0; k < t_string_len; ++k) {
+        while ((q > 0) && (string[q] != t_string[k])) {
+            q = pi[q - 1];
+        }
+        if (string[q] == t_string[k]) {
+            ++q;
+        }
+        if (q == 0) {
+            return false;
+        }
+        if (q == strlen(string)) {
+            q = pi[q - 1];
+        }
+    }
+    return true;
+}
+
+int main(int argc, char **argv) {
+    char *string = argv[1];
+    char *t_string = argv[2];
+    if (is_made_of_prefixes(string, t_string)) {
+        printf("yes");
+    } else {
+        printf("no");
+    }
+    return 0;
+}
